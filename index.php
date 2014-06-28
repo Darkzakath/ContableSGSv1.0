062302fec64da8cb049a1f87cf2616919541fd0b
Darkzakath <ezequiel.toloza@gmail.com>
2014-06-28 12:25:47 -0300

    index.php

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8" />
+	<meta description="Admiistración contable del Sindicato Grafico Sureño" />
+	<title>Sindicato Grafico Sureño: Administración</title>
+</head>
+<body>
+ 	<header>
+ 		<figure id="logo">
+ 			<img src="logo.png" />
+		</figure>
+		<h1>
+			Sindicato Grafico Sureño: Administración
+		</h1>
+		<figure id="avatar">
+			<img src="avatar.jpg" />
+		</figure>
+ 	</header>
+ 	<nav>
+ 		<ul>
+ 			<li><a href="#">Inicio</a></li>
+ 			<li><a href="#">Nomina de Trabajadores</a></li>
+ 			<li><a href="#">Escala Salarial</a></li>
+ 			<li><a href="#">Historia</a></li>
+ 			<li><a href="#">Noticias</a></li>
+ 			<li id="contacto_nav">
+ 				<a href="#">Contactanos</a>
+ 			</li>
+ 		</ul>
+ 	</nav>
+ 	<section id="contenido">
+ 		<article class="item">
+ 			<figure class="imagen_item">
+ 				<img src="imagen.jpg" />
+ 			</figure>
+ 			<h2 class="titulo_item">
+ 				<a href="#">
+ 					Título del articulo
+ 				</a>
+ 			</h2>
+ 			<div class="autor_item">
+ 				por <a href="#">Ezequiel Toloza</a>
+ 			</div>
+ 			<div class="datos_item">
+ 				<a href="#" class="tag_item"></a>
+ 				<span>
+ 					hace <strong>5</strong> min
+ 				</span>
+ 				<a href="#" class="comentarios_item">69</a>
+ 				<a href="#" class="guardar_item">Zeke</a>
+ 			</div>
+ 			<div class="votacion">
+ 				<a href="#" class="up"></a>
+ 				720
+ 				<a href="#" class="down"></a>
+ 			</div>
+ 		</article>
+ 	</section>
+ 	<footer>
+		<p>
+			<strong>Power by Zeke =)</strong>
+		</p>
+		<p>
+			Sindicato Grafico Sureño - 2014
+		</p>
+ 	</footer>
+</body>
+</html>
