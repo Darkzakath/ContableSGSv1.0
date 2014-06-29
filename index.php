af06295fe39c967d5cc16ac597f3e78102678ec2
Adrian <sanmartin.adrian@gmail.com>
2014-06-29 20:28:05 -0300

    First Commit
    Inicios

--- index.php
+++ index.php
@@ -2,68 +2,24 @@
 <html lang="es">
 <head>
 	<meta charset="utf-8" />
-	<meta description="Admiistración contable del Sindicato Grafico Sureño" />
+	<meta name="description" content="Admiistración contable del Sindicato Grafico Sureño" />
+	<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1" />
 	<title>Sindicato Grafico Sureño: Administración</title>
+
+	<link rel="stylesheet" href="librerias/css/normalize.css">
+	<link rel="stylesheet" href="estilos/media.css">
+	<link rel="stylesheet" href="estilos/main.css">
+
+	<script src="librerias/js/prefixfree.js"></script>
 </head>
 <body>
  	<header>
- 		<figure id="logo">
- 			<img src="logo.png" />
-		</figure>
-		<h1>
-			Sindicato Grafico Sureño: Administración
-		</h1>
-		<figure id="avatar">
-			<img src="avatar.jpg" />
-		</figure>
  	</header>
  	<nav>
- 		<ul>
- 			<li><a href="#">Inicio</a></li>
- 			<li><a href="#">Nomina de Trabajadores</a></li>
- 			<li><a href="#">Escala Salarial</a></li>
- 			<li><a href="#">Historia</a></li>
- 			<li><a href="#">Noticias</a></li>
- 			<li id="contacto_nav">
- 				<a href="#">Contactanos</a>
- 			</li>
- 		</ul>
  	</nav>
- 	<section id="contenido">
- 		<article class="item">
- 			<figure class="imagen_item">
- 				<img src="imagen.jpg" />
- 			</figure>
- 			<h2 class="titulo_item">
- 				<a href="#">
- 					Título del articulo
- 				</a>
- 			</h2>
- 			<div class="autor_item">
- 				por <a href="#">Ezequiel Toloza</a>
- 			</div>
- 			<div class="datos_item">
- 				<a href="#" class="tag_item"></a>
- 				<span>
- 					hace <strong>5</strong> min
- 				</span>
- 				<a href="#" class="comentarios_item">69</a>
- 				<a href="#" class="guardar_item">Zeke</a>
- 			</div>
- 			<div class="votacion">
- 				<a href="#" class="up"></a>
- 				720
- 				<a href="#" class="down"></a>
- 			</div>
- 		</article>
+ 	<section>
  	</section>
  	<footer>
-		<p>
-			<strong>Power by Zeke =)</strong>
-		</p>
-		<p>
-			Sindicato Grafico Sureño - 2014
-		</p>
  	</footer>
 </body>
 </html>
