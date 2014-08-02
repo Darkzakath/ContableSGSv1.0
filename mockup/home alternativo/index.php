b79567e80c8fea15d0fe061fe3afe4e3dd810b97
Adrian <sanmartin.adrian@gmail.com>
2014-08-01 23:44:45 -0300

    nav v1.1

--- mockup/home alternativo/index.php
+++ mockup/home alternativo/index.php
@@ -11,7 +11,7 @@
 	<script type="text/javascript" src="../../librerias/js/bootstrap.min.js"></script>
 	
 </head>
-<body>
+<body class="container">
 	<header>
 		<ul>
 			<li>logo</li>
@@ -25,7 +25,7 @@
 			<li>Menu</li>
 		</ul>
 	</nav-->
-	<nav class="navbar navbar-inverse" role="navigation">
+	<nav class="navbar navbar-default" role="navigation">
 		<!-- El logotipo y el icono que despliega el menú se agrupan
 		para mostrarlos mejor en los dispositivos móviles -->
 		<div class="navbar-header">
@@ -38,79 +38,51 @@
 			</button>
 			<a class="navbar-brand" href="#">Nombre de la empresa</a>
 		</div>
-
-		<!-- Agrupar los enlaces de navegación, los formularios y cualquier
-		otro elemento que se pueda ocultar al minimizar la barra -->
-		<div class="collapse navbar-collapse navbar-ex1-collapse">
-			<ul class="nav navbar-nav navbar-right">
-				<li class="active">
- 					<a href="#">Nomina de Trabajadores</a>
- 				</li>
- 				<li>
- 					<a href="#">Estudio Contable - Datos</a>
- 				</li>
- 				<li>
- 					<a href="#">Impresión de Boletas</a>
- 				</li>
- 				<li>
- 					<a href="#">Escala Salarial - Sector Obra</a>
- 				</li>
- 				<li>
- 					<a href="#">Escala Salarial - Zona Patagonica</a>
- 				</li>
- 				<li>
- 					<a href="#">Escala Salarial - Sector Diario</a>
- 				</li>
- 				<li>
- 					<a href="#">Horas Exedentes</a>
- 				</li>
- 				<li>
- 					<a href="#">Ospida - Obra Social</a>
- 				</li>
- 				<li>
- 					<a href="#">Historial de Bolestas</a>
- 				</li>
- 				<li>
- 					<a href="#">Tutorial</a>
- 				</li>
-				<!--li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-					Menú #1 <b class="caret"></b>
-					</a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Acción #1</a></li>
-						<li><a href="#">Acción #2</a></li>
-						<li><a href="#">Acción #3</a></li>
-						<li class="divider"></li>
-						<li><a href="#">Acción #4</a></li>
-						<li class="divider"></li>
-						<li><a href="#">Acción #5</a></li>
-					</ul>
-				</li-->
-			</ul>
-		</div>
 	</nav>
  	<section>
  		<article>
- 			<select id="cd-dropdown" class="cd-select">
- 				<option value="-1" selected>Choose an animal</option>
- 				<option value="1" class="icon-monkey">Monkey</option>
- 				<option value="2" class="icon-bear">Bear</option>
- 				<option value="3" class="icon-squirrel">Squirrel</option>
- 				<option value="4" class="icon-elephant">Elephant</option>
- 			</select> 
+ 			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav navbar-right">
+	 				<li>
+	 					<a href="#">Nomina de Trabajadores</a>
+	 				</li>
+	 				<li>
+	 					<a href="#">Estudio Contable - Datos</a>
+	 				</li>
+	 				<li>
+	 					<a href="#">Impresión de Boletas</a>
+	 				</li>
+	 				<li class="dropdown">
+	 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+		 					Escala Salarial
+		 					<b class="caret"></b>
+	 					</a>
+	 					<ul class="dropdown-menu">
+	 						<li><a href="#">Sector Obra</a></li>
+	 						<li><a href="#">Zona Patagonica</a></li>
+	 						<li><a href="#">Sector Diario</a></li>
+	 					</ul>
+	 				</li>
+	 				<li>
+	 					<a href="#">Horas Exedentes</a>
+	 				</li>
+	 				<li>
+	 					<a href="#">Ospida - Obra Social</a>
+	 				</li>
+	 				<li>
+	 					<a href="#">Historial de Bolestas</a>
+	 				</li>
+	 				<li>
+	 					<a href="#">Tutorial</a>
+	 				</li>
+	 			</ul>
+	 		</div>
+ 			<div id="cosa1">cosa loca</div>
+ 			<script type="text/javascript">
+ 				$('#cosa1').animate({'margin-left':'600px'},11500);
+ 			</script>
  		</article>
  	</section>
  	<footer></footer>
- 	<script type="text/javascript">
-			
-			$( function() {
-				
-				$( '#cd-dropdown' ).dropdown( {
-					gutter : 5
-				} );
-
-			});
-	</script>
 </body>
 </html>
