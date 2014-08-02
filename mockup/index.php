f70aa31b9a33d3a18204a24e7742c984bb614fc0
Darkzakath <ezequiel.toloza@gmail.com>
2014-08-01 23:16:38 -0300

    Arreglos en el nav

--- mockup/index.php
+++ mockup/index.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1" />
 	<title>Sindicato Grafico Sureño: Gestión</title>
 
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,700" type="text/css">
 	<link rel="stylesheet" href="../librerias/css/normalize.css"><!--Regula el css en todos los exploradores-->
 	<link rel="stylesheet" href="../librerias/css/bootstrap-theme.css">
 	<link rel="stylesheet" href="../librerias/css/bootstrap-theme.css.map">
@@ -15,41 +16,52 @@
 	<link rel="stylesheet" href="../librerias/css/bootstrap.min.css">
 	<link rel="stylesheet" href="../estilos/media.css">
 	<link rel="stylesheet" href="../estilos/main.css">
-
 	<script src="../librerias/js/prefixfree.js"></script>
 	
 </head>
 <body>
  	<header class="row">
  		<figure id="logo">
- 			<img src="../imagenes/sgs-logo.png" />
+ 			<img src="../imagenes/sgs-logo.png" >
  		</figure>
  		<!-- Hacer click en el logo te lleva a la pagina de inicio -->
  		<div class="contacto">
  			<a href="#">Contacto</a>
  		</div>
  	</header>
- 	<nav class="navbar-collapse collapse">
- 		<ul claas="nav nav-justified">
- 			<li>
- 				<figure id="avatar">
-		 			<img src="#" /> 
-		 		</figure>
- 				<ul id="edit-datos" class="no-visible">
- 					<li>Nombre</li>
-	 				<li>Cuit</li>
-	 				<li>Dirección</li>
-	 				<li>Telefono</li>
-	 				<li>E-mail</li>
- 				</ul>
- 			</li>
- 			<li>Anuncios y Notificaciones</li>
- 			<li>Menú</li>
- 		</ul>
+ 	<nav class="navbar navbar-inverse" role="navigation">
+ 		<div class="navbar-header">
+ 			<ul claas="nav">
+				<button type="button" class="navbar-toggle" data-toggle="collapse"
+					data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Desplegar navegación</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+	 			<li>
+	 				<figure id="avatar">
+			 			<img src="#" > 
+			 		</figure>
+			 		<div id="edit-datos">
+		 				<ul class="no-visible">
+		 					<li>Nombre</li>
+			 				<li>Cuit</li>
+			 				<li>Dirección</li>
+			 				<li>Telefono</li>
+			 				<li>E-mail</li>
+		 				</ul>
+	 				</div>
+	 			</li>
+	 			<li>Anuncios y Notificaciones</li>
+	 			<li>Menú</li>
+ 			</ul>
+		</div>
  	</nav>
  	<section>
  		<article id="submenu" class="no-visible">
- 			<ul>
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<ul class="nav navbar-nav navbar-right">
  				<li>
  					<a href="#">Nomina de Trabajadores</a>
  				</li>
@@ -82,7 +94,7 @@
  				</li>
 
  			</ul>
- 		</article>
+ 		</div>
  		<article class="item">
  			<h2 class="titulo_item">
  				<a href="#">Titulo</a>
@@ -94,7 +106,7 @@
  		<p>Power by Molleja&Zeke</p>
  		<p>Sindicato Grafico Sureño - 2014</p>
  	</footer>
-
+	<script src="../librerias/js/jquery-2.1.1.min.js"></script>
  	<script src="../librerias/js/bootstrap.min.js"></script>
 	<script src="../librerias/js/bootstrap.js"></script>
 </body>
